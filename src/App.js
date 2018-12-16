67c7cbab71c451f158cd5612f12c21a5f46a6da7
Manuel Jung <manueljung@Manuels-MacBook-Pro.local>
2018-12-16 21:19:13 +0100

    init

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import * as React from 'react'
 import {Provider} from 'react-redux'
 
 import {createProduct} from 'modules/products/actions'
+import {Filter} from 'modules/products/hocs/withFilter'
 
 export default class App extends React.Component {
-  componentDidMount(){
-    this.props.store.dispatch(createProduct('10082660', 'displayProduct'))
-  }
   render() {
     return (
       <Provider store={this.props.store}>
         <div className="App">
-          APP
         </div>
       </Provider>
     )
--- src/modules/products/utils/api.js
+++
@@ -1,9 +0,0 @@
-
-
-export function fetchProduct(number){
-  return fetch('http://localhost:3001/container/'+number)
-    .then(res => res.json())
-    .then(productNumber => fetch('http://localhost:3001/variants/'+productNumber))
-    .then(res => res.json())
-    .then(result => result.articles)
-}
