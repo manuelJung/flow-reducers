ac69fd3c2d8be492805e6d77915e1e064f4ec799
Manuel Jung <manueljung@Manuels-MacBook-Pro.local>
2018-12-18 09:02:02 +0700

    hoc can fetch

--- src/modules/staticBlocks/hocs/withStaticBlock.js
+++ src/modules/staticBlocks/hocs/withStaticBlock.js
@@ -4,46 +4,65 @@ import {connect} from 'react-redux'
 
 import type {RootState} from 'store/rootReducer'
 import type {Identifier, StaticBlock as StaticBlockType} from '../entities'
-import {isFetching, getFetchError, getStaticBlock} from '../selectors'
+import {isFetching, getFetchError, getStaticBlock, shouldFetch} from '../selectors'
+import {fetchRequest} from '../actions'
 
 type InjectedProps = {
   isFetching: boolean,
   fetchError: string | null,
-  block: StaticBlockType | null
+  block: StaticBlockType | null,
+  shouldFetch: boolean,
+  fetch: () => void
 }
 
 type RequiredProps = {
   identifier: Identifier,
-  render?: (props:$Diff<Result,{render:any}>) => any
+  render?: (props:$Diff<AllProps,{render:any}>) => any
 }
 
-type Result = {
+type AllProps = {
   isFetching: boolean,
   fetchError: string | null,
   block: StaticBlockType | null,
   identifier: Identifier,
-  render?: (props:$Diff<Result,{render:any}>) => any
+  shouldFetch: boolean,
+  fetch: () => void,
+  render?: (props:$Diff<AllProps,{render:any}>) => any
 }
 
 type Hoc = (
-  Component:React.ComponentType<Result>
+  Component:React.ComponentType<AllProps>
 ) => React.ComponentType<RequiredProps>
 
-function mapProps (state:RootState,{identifier}):InjectedProps {
+function mapProps (state:RootState,{identifier}) {
   return {
     isFetching: isFetching(state.staticBlocks, identifier),
     fetchError: getFetchError(state.staticBlocks, identifier),
-    block: getStaticBlock(state.staticBlocks, identifier)
+    block: getStaticBlock(state.staticBlocks, identifier),
+    shouldFetch: shouldFetch(state.staticBlocks, identifier)
   }
 }
 
-const mapDispatch = {}
+const mapDispatch = {fetchRequest}
 
-const hoc:Hoc = connect(mapProps, mapDispatch)
+function mergeProps (stateProps, {fetchRequest}, ownProps) {
+  return Object.assign({}, stateProps, ownProps, {
+    fetch: () => fetchRequest(ownProps.identifier)
+  })
+}
+
+const hoc:Hoc = connect(mapProps, mapDispatch, mergeProps)
 
 export default hoc
 
-export const StaticBlock = hoc(function StaticBlock ({render, ...props}){
-  return render ? render(props) : null
+
+export const StaticBlock = hoc(class StaticBlock extends React.Component<AllProps> {
+  componentDidMount(){
+    if(this.props.shouldFetch) this.props.fetch()
+  }
+  render() {
+    const {render, ...props} = this.props
+    return render ? render(props) : null
+  } 
 })
 
