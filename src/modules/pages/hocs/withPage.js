3871d5d0a22e0ed27084815b1eab1979d1b77664
Manuel Jung <manueljung@Manuels-MacBook-Pro.local>
2018-12-18 09:10:32 +0700

    updated withPage hoc

--- src/modules/pages/hocs/withPage.js
+++ src/modules/pages/hocs/withPage.js
@@ -4,45 +4,64 @@ import {connect} from 'react-redux'
 
 import type {RootState} from 'store/rootReducer'
 import type {Slug, Page as PageType} from '../entities'
+import {isFetching, getFetchError, getPage, shouldFetch} from '../selectors'
+import {fetchRequest} from '../actions'
 
 type InjectedProps = {
   isFetching: boolean,
   fetchError: string | null,
-  block: PageType | null
+  page: PageType | null,
+  shouldFetch: boolean,
+  fetch: () => void
 }
 
 type RequiredProps = {
   slug: Slug,
-  render?: (props:$Diff<Result,{render:any}>) => React.StatelessFunctionalComponent<$Diff<Result,{render:any}>>
+  render?: (props:$Diff<AllProps,{render:any}>) => any
 }
 
-type Result = {
+type AllProps = {
   isFetching: boolean,
   fetchError: string | null,
-  block: PageType | null,
+  page: PageType | null,
+  shouldFetch: boolean,
+  fetch: () => void,
   slug: Slug,
-  render?: (props:$Diff<Result,{render:any}>) => React.StatelessFunctionalComponent<$Diff<Result,{render:any}>>
+  render?: (props:$Diff<AllProps,{render:any}>) => any
 }
 
 type Hoc = (
-  Component:React.ComponentType<Result>
-) => React.ComponentType<Result>
+  Component:React.ComponentType<AllProps>
+) => React.ComponentType<AllProps>
 
-function mapProps (state:RootState,{slug}):InjectedProps {
+function mapProps (state:RootState,{slug}) {
   return {
-    isFetching: false,
-    fetchError: null,
-    block: null
+    isFetching: isFetching(state.pages, slug),
+    fetchError: getFetchError(state.pages, slug),
+    page: getPage(state.pages, slug),
+    shouldFetch: shouldFetch(state.pages, slug)
   }
 }
 
-const mapDispatch = {}
+const mapDispatch = {fetchRequest}
 
-const hoc:Hoc = connect(mapProps, mapDispatch)
+function mergeProps (stateProps, {fetchRequest}, ownProps) {
+  return Object.assign({}, stateProps, ownProps, {
+    fetch: () => fetchRequest(ownProps.slug)
+  })
+}
+
+const hoc:Hoc = connect(mapProps, mapDispatch, mergeProps)
 
 export default hoc
 
-export const Page = hoc(function Page ({render, ...props}){
-  return render ? render(props) : null
+export const Page = hoc(class Page extends React.Component<AllProps> {
+  componentDidMount(){
+    if(this.props.shouldFetch) this.props.fetch()
+  }
+  render() {
+    const {render, ...props} = this.props
+    return render ? render(props) : null
+  } 
 })
 
--- src/modules/pages/selectors.js
+++ src/modules/pages/selectors.js
@@ -2,7 +2,7 @@
 import type {State} from './reducer'
 import type {Slug, Page} from './entities'
 
-export const hasPage = (state:State, slug:Slug):boolean => Boolean(state[slug])
+export const shouldFetch = (state:State, slug:Slug):boolean => !state[slug]
 export const isFetching = (state:State, slug:Slug):boolean => state[slug] && state[slug].isFetching
 export const getFetchError = (state:State, slug:Slug):string|null => state[slug] ? state[slug].fetchError : null
 export const getPage = (state:State, slug:Slug):Page|null => state[slug] ? state[slug].page : null
