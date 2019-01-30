3f4a74d17dda053c32a86718c8bd9707fa3b476e
manuelJung <manu.jung@relaxcommerce.com>
2019-01-30 16:09:04 +0100

    flow typed ProductSearch

--- src/theme/organisms/ProductSearch/ProductSearch.js
+++ src/theme/organisms/ProductSearch/ProductSearch.js
@@ -4,9 +4,11 @@ import SearchInitializer from 'modules/products/hocs/ListInitializer'
 
 import SearchFilters from './SearchFilters'
 
+import type {FilterValues} from 'modules/products/entities'
+
 type Props = {
   identifier: string,
-  filters?: *
+  filters?: $Shape<FilterValues>
 }
 
 export default React.memo<Props>(function ProductSearch ({identifier, filters}:Props) {
