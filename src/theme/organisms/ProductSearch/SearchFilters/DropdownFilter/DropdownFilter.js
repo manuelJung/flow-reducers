14cb2610391cc1620cec5cd8831576a98a784691
manuelJung <manu.jung@relaxcommerce.com>
2019-01-30 17:19:24 +0100

    refactored DropdownFilter

--- src/theme/organisms/ProductSearch/SearchFilters/DropdownFilter/DropdownFilter.js
+++ src/theme/organisms/ProductSearch/SearchFilters/DropdownFilter/DropdownFilter.js
@@ -2,25 +2,34 @@
 import React from 'react'
 import type {Node} from 'react'
 import Dropdown from 'theme/molecules/Dropdown'
+import ListFilter from 'modules/products/hocs/ListFilter'
 import {dispatchEvent} from 'redux-interrupt'
 
+import type {FilterKey} from 'modules/products/entities'
+
 type Props = {
+  identifier: string,
   label: string,
-  filterKey: string,
-  children: Node
+  filterKey: FilterKey
 }
 
-export default React.memo<Props>(function ({label, filterKey, children}:Props){
+export default React.memo<Props>(function ({label, filterKey, identifier}:Props){
   return (
     <div className='DropdownFilter'>
     <Dropdown 
-      // onOpen={() => dispatchEvent({type: 'DropdownFilter/OPEN_DROPDOWN', meta: {filterKey} })}
-      // onClose={() => dispatchEvent({type: 'DropdownFilter/CLOSE_DROPDOWN', meta: {filterKey} })}
+      onOpen={() => dispatchEvent({type: 'DropdownFilter/OPEN_DROPDOWN', meta: {filterKey} })}
+      onClose={() => dispatchEvent({type: 'DropdownFilter/CLOSE_DROPDOWN', meta: {filterKey} })}
       label={label}
       render={() => (
-        <div className='content'>
-          {children}
-        </div>
+        <ListFilter pure identifier={identifier} filterKey={filterKey} render={props => (
+          <div className='content'>
+            {props.data.options.map(opt => (
+              <div key={opt} className='option' onClick={() => props.toggleOption(opt)}>
+                {opt}
+              </div>
+            ))}
+          </div>
+        )}/>
       )}
     />
     </div>
--- src/theme/organisms/ProductSearch/SearchFilters/SearchFilters.js
+++ src/theme/organisms/ProductSearch/SearchFilters/SearchFilters.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react'
-import ListFilter from 'modules/products/hocs/ListFilter'
 import {dispatchEvent} from 'redux-interrupt'
 
 import DropdownFilter from './DropdownFilter'
@@ -13,24 +12,8 @@ export default React.memo<Props>(function SearchFilters ({identifier}:Props) {
   const handleOpen = () => dispatchEvent({type: 'OPEN_FILTER', payload: 'color'})
   return (
     <div className='SearchFilters'>
-      <ListFilter pure identifier={identifier} filterKey='color' render={props => (
-        <DropdownFilter label='Farbe' filterKey='color'>
-          {props.data.options.map(opt => (
-            <div key={opt} className='option' onClick={() => props.toggleOption(opt)}>
-              {opt}
-            </div>
-          ))}
-        </DropdownFilter>
-      )} />
-      <ListFilter pure identifier={identifier} filterKey='brand' render={props => (
-        <DropdownFilter label='Brand' filterKey='color'>
-          {props.data.options.map(opt => (
-            <div key={opt} className='option' onClick={() => props.toggleOption(opt)}>
-              {opt}
-            </div>
-          ))}
-        </DropdownFilter>
-      )} />
+      <DropdownFilter label='Farbe' filterKey='color' identifier={identifier}/>
+      <DropdownFilter label='Brand' filterKey='brand' identifier={identifier}/>
     </div>
   )
 })
