0af89332162b0974c56e9b68f9328a55a7e17aee
manuelJung <manu.jung@relaxcommerce.com>
2019-01-31 17:46:52 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {Provider} from 'react-redux'
 import Component from './Component'
 import { ConnectedRouter } from 'react-router-redux'
 import {Switch, Route} from 'react-router'
+import { Link } from 'react-router-dom'
 
 import PageRoute from 'theme/pages/PageRoute'
 import MagazinListRoute from 'theme/pages/MagazinListRoute'
 import SearchRoute from 'theme/pages/SearchRoute'
+import CategoryRoute from 'theme/pages/CategoryRoute'
 
 import history from 'store/history'
 
@@ -18,12 +20,25 @@ export default class App extends React.Component<{store:any}> {
       <Provider store={this.props.store}>
         <ConnectedRouter history={history}>
           <div className="App">
-            <Switch>
-              <Route exact path='/' component={Component} />
-              <Route path='/page' component={PageRoute} />
-              <Route path='/magazin' component={MagazinListRoute} />
-              <Route path='/search' component={SearchRoute} />
-            </Switch>
+            <div className='navi'>
+              <Link to='/page'>Page </Link>
+              <Link to='/magazin'>Magazin </Link>
+              <Link to='/search'>Search </Link>
+              <Link to='/fashion/bekleidung'>fashion-1 </Link>
+              <Link to='/fashion/bekleidung/kleider'>fashion-2 </Link>
+              <Link to='/fashion/bekleidung/kleider/abendkleider'>fashion-3 </Link>
+            </div>
+            <div className='routes'>
+              <Switch>
+                <Route exact path='/' component={Component} />
+                <Route exact path='/page' component={PageRoute} />
+                <Route exact path='/magazin' component={MagazinListRoute} />
+                <Route exact path='/search' component={SearchRoute} />
+                <Route exact path='/fashion/:lv1' component={CategoryRoute} />
+                <Route exact path='/fashion/:lv1/:lv2' component={CategoryRoute} />
+                <Route exact path='/fashion/:lv1/:lv2/:lv3' component={CategoryRoute} />
+              </Switch>
+            </div>
           </div>
         </ConnectedRouter>
       </Provider>
--- src/theme/pages/CategoryRoute.js
+++ src/theme/pages/CategoryRoute.js
@@ -1,19 +1,20 @@
 // @flow
 import React from 'react'
 import ProductSearch from 'theme/organisms/ProductSearch'
-import StaticBlock from 'theme/organisms/StaticBlock'
+import CategoryContext from 'theme/organisms/CategoryContext'
 
 import Helmet from 'react-helmet'
 
 type Props = {}
 
-export default React.memo<Props>(function SearchRoute () {
+export default React.memo<Props>(function SearchRoute (props:Props) {
+  console.log(props)
   return (
     <div className='SearchRoute'>
       <Helmet title='Category-Route'/>
-      {/* <StaticBlock identifier='Sale_Category_Text1' />
-      <ProductSearch identifier='sale-route'/>
-      <StaticBlock identifier='Sale_Category_SEO_Text' /> */}
+      {/* <CategoryContext identifier='Sale_Category_Text1' /> */}
+      {/* <ProductSearch identifier='sale-route'/> */}
+      {/* <StaticBlock identifier='Sale_Category_SEO_Text' /> */}
     </div>
   )
 })
