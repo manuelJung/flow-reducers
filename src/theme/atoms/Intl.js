91794a046de3be7549384d623ade598598c05f51
manuelJung <manu.jung@relaxcommerce.com>
2019-02-01 18:09:52 +0100

    created Intl atom

---
+++ src/theme/atoms/Intl.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import type {Node, Component} from 'react'
+import { injectIntl } from 'react-intl'
+
+type Intl = {
+  formatMessage: (
+    {id:string},
+    values: {[key:string]: string}
+  ) => string
+}
+
+type Props = {| 
+  children: (intl:Intl) => Node 
+|} | {| 
+  id: string, 
+  values?: {[key:string]: string} 
+|}
+
+const IntlComponent:Component<Props> = injectIntl(function Intl(props:{...Props, intl:Intl}){
+  if(props.children) return props.children(props.intl)
+  return props.intl.formatMessage({id: props.id}, props.values)
+})
+
+export default IntlComponent
