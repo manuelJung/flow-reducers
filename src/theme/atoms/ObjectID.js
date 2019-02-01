35c380b8dbcbe861f5d91292007e63e214234a81
manuelJung <manu.jung@relaxcommerce.com>
2019-02-01 18:21:10 +0100

    created ObjectID atom

---
+++ src/theme/atoms/ObjectID.js
@@ -0,0 +1,22 @@
+// @flow
+import type {Component} from 'react'
+import styled from 'styled-components'
+import {adminMode} from 'config'
+
+type Props = {|
+  children: string
+|}
+
+const ObjectID:Component<Props> = styled.code.attrs({
+  onClick: (e:any) => e.stopPropagation()
+})`
+  &:before { content: "Object-ID: "; }
+  display: ${adminMode ? 'block' : 'none'};
+  text-align: center;
+  color: #993452;
+  background: #eeeced;
+  font-size: 10px;
+  padding: 2px;
+`
+
+export default ObjectID
